8c86c21d1f43789b8f3de9ff65914bdedc2c7fa7
Nick Cramaro <nickcramaro@gmail.com>
2020-10-25 12:48:19 -0400

    Made app function, playing around with stripe

--- pages/_app.js
+++ pages/_app.js
@@ -1,63 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cookie from 'js-cookie';
-import fetch from 'isomorphic-fetch';
-import App from 'next/app';
 
 import Layout from '../components/Layout';
 import AppContext from '../context/AppContext';
 import withApollo from '../lib/apollo';
+import to from '../lib/to';
 
 import '../styles/global.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
-class CNCApp extends App {
-	state = {
-		user: null,
-	};
+const App = ({ Component, pageProps }) => {
+  const [user, setUser] = useState(null); 
 
-	componentDidMount() {
-		const token = Cookie.get('token');
+  useEffect(async () => {
+    const token = Cookie.get('token');
 
 		if (token) {
 			// authenticate the token on the server and place set user object
-			fetch('http://localhost:1337/users/me', {
+			const {data: res, error: err} = await to(fetch('http://localhost:1337/users/me', {
 				headers: {
 					Authorization: `Bearer ${token}`,
 				},
-			}).then(async (res) => {
+			}));
+			
+			if (res) {
 				// if res comes back not valid, token is not valid
 				// delete the token and log the user out on client
 				if (!res.ok) {
 					Cookie.remove('token');
-					this.setState({ user: null });
-					return null;
+          setUser(null);
+          return null;
 				}
 				const user = await res.json();
-				this.setUser(user);
-			});
+				setUser(user);
+			}
 		}
-	}
+  }, []);
 
-	setUser = (user) => {
-		this.setState({ user });
-	};
-
-	render() {
-		const { Component, pageProps } = this.props;
-
-		return (
-			<AppContext.Provider
+  return (
+    <AppContext.Provider
 				value={{
-					user: this.state.user,
-					isAuthenticated: !!this.state.user,
-					setUser: this.setUser,
+					user: user,
+					isAuthenticated: !!user,
+					setUser: setUser,
 				}}>
 				<Layout>
 					<Component {...pageProps} />
 				</Layout>
 			</AppContext.Provider>
-		);
-	}
-}
+  )
+};
 
-export default withApollo({ ssr: true })(CNCApp);
+export default withApollo({ ssr: true })(App);
