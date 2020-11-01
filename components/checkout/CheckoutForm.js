7b5a159d4217ce0d7ac6614571911808aeb91161
Nick Cramaro <nickcramaro@gmail.com>
2020-11-01 12:08:48 -0500

    Create membership

--- components/checkout/CheckoutForm.js
+++ components/checkout/CheckoutForm.js
@@ -1,11 +1,10 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 
 import fetch from 'isomorphic-fetch';
 import Cookies from 'js-cookie';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 import CardSection from './CardSection';
-import AppContext from '../../context/AppContext';
 
 const CheckoutForm = () => {
 	const [data, setData] = useState({
@@ -17,7 +16,6 @@ const CheckoutForm = () => {
 	const [error, setError] = useState('');
 	const stripe = useStripe();
 	const elements = useElements();
-	const appContext = useContext(AppContext);
 
 	function onChange(e) {
 		// set the key = to the name property equal to the value typed
@@ -30,26 +28,26 @@ const CheckoutForm = () => {
 		// // Use elements.getElement to get a reference to the mounted Element.
 		const cardElement = elements.getElement(CardElement);
 
-		// // Pass the Element directly to other Stripe.js methods:
-		// // e.g. createToken - https://stripe.com/docs/js/tokens_sources/create_token?type=cardElement
 		// get token back from stripe to process credit card
 		const token = await stripe.createToken(cardElement);
+
+		if (token.error) {
+			setError(token.error.message);
+			return;
+		}
+
 		const userToken = Cookies.get('token');
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders`, {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/memberships`, {
 			method: 'POST',
 			headers: userToken && { Authorization: `Bearer ${userToken}` },
 			body: JSON.stringify({
-				amount: Number(Math.round(appContext.cart.total + 'e2') + 'e-2'),
-				dishes: appContext.cart.items,
-				address: data.address,
-				city: data.city,
-				state: data.state,
 				token: token.token.id,
 			}),
 		});
 
 		if (!response.ok) {
 			setError(response.statusText);
+			return;
 		}
 	}
 
