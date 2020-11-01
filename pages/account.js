f464ef2bb767ffa0414b7b27334799d0a139fff5
Nick Cramaro <nickcramaro@gmail.com>
2020-11-01 14:40:42 -0500

    Display membership number or stripe

--- pages/account.js
+++ pages/account.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
+import AppContext from '../context/AppContext';
 import { requireAuth } from '../lib/auth';
 import InjectedCheckoutForm from '../components/checkout/CheckoutForm';
 
+const GET_MEMBERSHIP = gql`
+	query($id: ID!) {
+		membership(id: $id) {
+			number
+			created_at
+		} 
+	}
+`;
+
 const Account = () => {
-	const STRIPE_API_KEY = process.env.NEXT_PUBLIC_STRIPE_API_KEY;
+	const appContext = useContext(AppContext);
 
-	// load stripe to inject into elements components
-	const stripePromise = loadStripe(STRIPE_API_KEY);
+	const { loading, error, data } = useQuery(GET_MEMBERSHIP, {
+		variables: { id: appContext.user && appContext.user.membership },
+	});
 
-	return (
-		<div>
-			<div className="container__section container__section--blue">
-				<h1>Account Settings</h1>
+	if (loading) return <h1>Fetching membership</h1>;
+
+	if (data) {
+		console.log(data);
+		return (
+			<div>
+				<div className="container__section container__section--blue">
+					<h1>Account Settings</h1>
+				</div>
+				<div className="container__section">
+					Membership Number: {data.membership.number}
+				</div>
 			</div>
-			<div className="container__section">
-				<Elements stripe={stripePromise}>
-					<InjectedCheckoutForm />
-				</Elements>
+		);
+	} else if (appContext.user && appContext.user.membership && error) {
+		return 'Error fetching membership';
+	} else {
+		const STRIPE_API_KEY = process.env.NEXT_PUBLIC_STRIPE_API_KEY;
+
+		// load stripe to inject into elements components
+		const stripePromise = loadStripe(STRIPE_API_KEY);
+
+		return (
+			<div>
+				<div className="container__section container__section--blue">
+					<h1>Account Settings</h1>
+				</div>
+				<div className="container__section">
+					<Elements stripe={stripePromise}>
+						<InjectedCheckoutForm />
+					</Elements>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export const getServerSideProps = requireAuth();
